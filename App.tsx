bbb8a4fa0bf02ba929c01a4343069bdaeb9c7caf
Kaiser <duy2172003@gmail.com>
2026-01-19 20:25:11 +0700

    chore: remove import

--- App.tsx
+++ App.tsx
@@ -1,23 +1,18 @@
 /**
  * Sample React Native App
- * https://github.com/facebook/react-native
+ * https:
  *
  * @format
  */
 
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import React, {useEffect} from 'react';
-import type {PropsWithChildren} from 'react';
-import {ScrollView, StatusBar, Text, useColorScheme, View} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import RootNavigation from './src/components/navigation/RootNavigation';
 import initI18n from './src/i18n/config';
 import {LLMProvider} from './src/components/provider/LLMProvider';
-import {
-  initDatabase,
-  deleteAndRecreateDatabase,
-} from './src/database/dataSource';
+import { initDatabase } from './src/database/dataSource';
 import 'react-native-get-random-values';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from '@/lib/react-query';
@@ -25,7 +20,6 @@ initI18n();
 
 function App(): React.JSX.Element {
   useEffect(() => {
-    // Initialize database without deleting existing data
     initDatabase()
       .then(() => console.log('✅ Database initialized'))
       .catch(error => console.error('❌ Database init error:', error));
